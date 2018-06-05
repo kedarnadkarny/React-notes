1123658d30e5aff9ca679f4cb0ceff2e619bfc96
Kedar <kedar.nadkarny@gmail.com>
2018-06-04 23:23:42 -0500

    config

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "XXXXXXXXXXX-XXXXXXXXXXX",
+    authDomain: "project.yourapp.com",
+    databaseURL: "URLtoYourDatabase",
+    projectId: "projectID",
+    storageBucket: "BucketURL",
+    messagingSenderId: "messagingId"
+};
